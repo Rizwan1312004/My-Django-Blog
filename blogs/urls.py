1fcf9c50314e795976c47cdb446f594b4beef6ce
Rizwan m <rizwan1312004@gmail.com>
2026-01-02 08:45:59 +0530

    main page and selection

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Home, name = 'heading'),
+    path('login/', views.LoginUser, name='login'),
+    path('<int:category_id>/', views.CategoryBlogs, name='category_blogs'),
+    path('<slug:slug>/', views.BlogDetail, name='blog_detail'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,39 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Blog
 # Create your views here.
 
 def Home(request):
-    return render(request, 'Home.html')
+    categories = Category.objects.all()
+    featured_blogs = Blog.objects.filter(is_featured=True, status='published').order_by('-created_at')
+    posts = Blog.objects.filter(is_featured=False, status='published').order_by('-created_at')
+    context = {
+        'categories': categories,
+        'featured_blogs': featured_blogs,
+        'posts': posts,
+        }
+    return render(request, 'Home.html', context)
+
+def CategoryBlogs(request, category_id):
+    categories = Category.objects.all()
+    category = get_object_or_404(Category, id=category_id)
+    featured_blogs = Blog.objects.filter(category=category,is_featured=True, status='published',).order_by('-created_at')
+    posts = Blog.objects.filter(category=category, is_featured=False, status='published').order_by('-created_at')
+    context = {
+        'categories': categories,
+        'category': category,
+        'cat_posts': posts,
+        'featured_blogs': featured_blogs,
+    }
+    return render(request, 'Home.html', context)
+
+def BlogDetail(request, slug):
+    blog_post = get_object_or_404(Blog, slug=slug, status='published')
+    related_blogs = Blog.objects.filter(category=blog_post.category, status='published',).order_by('-created_at').exclude(slug=blog_post.slug)
+    context ={
+        'blog_post': blog_post,
+        'related_blogs': related_blogs,
+    }
+    return render(request, 'main.html', context)
+
+def LoginUser(request):
+    return render(request, 'login.html')
